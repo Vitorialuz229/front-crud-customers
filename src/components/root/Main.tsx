6e92e448615f0af35c8bebce70c08c5bfc748454
Vitorialuz229 <vitorialuz831@gmail.com>
2025-02-02 22:07:54 -0300

    :bug: fix: Correção de bug

---
+++ src/components/root/Main.tsx
@@ -0,0 +1,10 @@
+function Main() {
+  return (
+    <div className="text-center text-3xl text-red-500">
+      <h1>Home</h1>
+      <p>Welcome to the Home page!</p>
+    </div>
+  );
+}
+
+export default Main;
